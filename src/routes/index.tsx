ccfa5a9c1093ad2d20b6a6ef576227ea95b81aa8
yash-dev07 <yashpatel.yp2005@gmail.com>
2026-08-22 00:28:05 +0530

    update recharts + fix build issues

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -159,7 +159,7 @@ function BackgroundRemover() {
     setResultUrl(null);
     const startedAt = performance.now();
     try {
-      const mod = await import("@/lib/remove-bg.client");
+      const mod = await loadRemoveBg();
       const { blob, source } = (await mod.removeBackgroundSmart(
         selectedFile,
         webhookRemove as RemoveBgServerFn,
